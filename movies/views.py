20d1f44945285bb9198e5d99a8c00ad65cf70eff
DariuszSlowinski <dariusz.slowinski@uwm.edu.pl>
2026-04-11 21:04:14 +0200

    Wykład 02: JWT i zabezpieczenie API

--- movies/views.py
+++ movies/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
 from .models import Movie
 from .serializers import MovieSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all().order_by('-id')
     serializer_class = MovieSerializer
+    permission_classes = [IsAuthenticated]
 
 
--- zai_api/settings.py
+++ zai_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,8 +119,17 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-ERST_FRAMEWORK = {
+REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'AUTH_HEADER_TYPES': ('Bearer',),
 }
